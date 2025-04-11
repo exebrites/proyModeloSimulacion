cdae1d9010a969d8004a4b305f219ce2b4520463
exe <exequielbrites8@gmail.com>
2025-04-11 16:56:32 -0300

    creacion de controladorfibonacci

---
+++ app/Http/Controllers/FibonacciController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FibonacciController extends Controller
+{
+
+
+    /*
+        NÚMEROS PSEUDOALEATORIOS
+        MÉTODO DE FIBONACCI
+        Consiste en:
+        1. Elegir 3 (tres) parámetros (enteros no negativos de 3 a 7
+        dígitos): V1, V2 y A, que se asignan como valores del
+        primer y segundo términos de la serie y de un parámetro de
+        control, respectivamente.
+
+        
+        2. Aplicar: V3 = V2 + V1 + k A, donde k es un parámetro real y
+        entero que se define como:
+        * 0 si V2 + V1 <= A
+        * -1 en otro caso
+        3. Asignar V3 a V2, y V2 a V1.
+        4. Repetir los pasos del 2 al 3 n veces, siendo n la cantidad de
+        nros pseudoaleatorios a generar.
+    */
+
+
+    public function metodoFibonacci(Request $request)
+    {
+        $v1 = $request->input('v1');
+        $v2 = $request->input('v2');
+        $a = $request->input('a');
+        $n = $request->input('n');
+        
+        //comienzo del bucle
+        // definir k 
+        /*
+        SI V2 + V1 <= A   ENTONCES k = 0
+        SINO k = -1
+        */
+
+        //asignar valores
+        //v2=v3
+        //v1=v2
+
+        // repetir n veces
+
+
+        
+    }
+}
